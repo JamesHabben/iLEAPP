9110c7edca453c6bb0ec6630e3ff70b9c4c7c92b
James Habben <james@wmif.net>
2023-10-24 14:52:11 -0700

    module updates

--- scripts/artifacts/AllTrails.py
+++ scripts/artifacts/AllTrails.py
@@ -2,7 +2,8 @@ import sqlite3
 import textwrap
 from datetime import datetime, timezone
 from scripts.artifact_report import ArtifactHtmlReport
-from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, get_next_unused_name, open_sqlite_db_readonly, convert_ts_human_to_utc, convert_utc_human_to_timezone
+from scripts.ilapfuncs import (logfunc, tsv, timeline, is_platform_windows, get_next_unused_name,
+                               open_sqlite_file_readonly, convert_sqlite_epoch)
 
 def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset):
     
@@ -11,34 +12,33 @@ def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset
         if not file_found.endswith('AllTrails.sqlite'):
             continue # Skip all other files
     
-        db = open_sqlite_db_readonly(file_found)
-        cursor = db.cursor()
+        cursor = open_sqlite_file_readonly(file_found)
         cursor.execute('''
-        Select 
-        ZTRAIL.ZNAME,
-        ZTRAIL.ZROUTETYPENAME,
-        CASE ZACTIVITYSTATS.ZDIFFICULTY
-            WHEN 1 THEN 'Easy'
-            WHEN 3 THEN 'Moderate'
-            WHEN 5 THEN 'Hard'
-        END,
-        ZTRAIL.ZRATING,
-        ZTRAIL.ZREVIEWCOUNT,
-        ZTRAIL.ZLENGTH as "Length (Meters)",
-        ZTRAIL.ZELEVATIONGAIN as "Elevation Gain (Meters)",
-        ZLOCATION.ZLATITUDE,
-        ZLOCATION.ZLONGITUDE,
-        ZLOCATION.ZCITY,
-        ZLOCATION.ZREGION,
-        ZLOCATION.ZREGIONNAME,
-        ZLOCATION.ZPOSTALCODE,
-        ZLOCATION.ZCOUNTRY,
-        ZLOCATION.ZCOUNTRYNAME,
-        ZPARKAREA.ZNAME as "Park Area Name"
-        From ZLOCATION
-        Join ZTRAIL On ZLOCATION.Z_PK = ZTRAIL.ZLOCATION
-        Join ZPARKAREA On ZTRAIL.Z_PK = ZPARKAREA.ZTRAIL
-        Join ZACTIVITYSTATS On ZTRAIL.Z_PK = ZACTIVITYSTATS.ZTRAIL
+            Select 
+            ZTRAIL.ZNAME, 
+            ZTRAIL.ZROUTETYPENAME,
+            CASE ZACTIVITYSTATS.ZDIFFICULTY
+                WHEN 1 THEN 'Easy'
+                WHEN 3 THEN 'Moderate'
+                WHEN 5 THEN 'Hard'
+            END,
+            ZTRAIL.ZRATING,
+            ZTRAIL.ZREVIEWCOUNT,
+            ZTRAIL.ZLENGTH,
+            ZTRAIL.ZELEVATIONGAIN,
+            ZLOCATION.ZLATITUDE,
+            ZLOCATION.ZLONGITUDE,
+            ZLOCATION.ZCITY,
+            ZLOCATION.ZREGION,
+            ZLOCATION.ZREGIONNAME,
+            ZLOCATION.ZPOSTALCODE,
+            ZLOCATION.ZCOUNTRY,
+            ZLOCATION.ZCOUNTRYNAME,
+            ZPARKAREA.ZNAME as zparkname
+            From ZLOCATION
+            Join ZTRAIL On ZLOCATION.Z_PK = ZTRAIL.ZLOCATION
+            Join ZPARKAREA On ZTRAIL.Z_PK = ZPARKAREA.ZTRAIL
+            Join ZACTIVITYSTATS On ZTRAIL.Z_PK = ZACTIVITYSTATS.ZTRAIL
         ''')
 
         all_rows = cursor.fetchall()
@@ -47,10 +47,30 @@ def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset
             report = ArtifactHtmlReport('AllTrails - Trail Details')
             report.start_artifact_report(report_folder, 'AllTrails - Trail Details')
             report.add_script()
-            data_headers = ('Trail Name','Route Type','Trail Difficulty','Rating','Review Count','Length (Meters)','Elevation Gain (Meters)','Latitude','Longitude','City','State/Region','State/Region Name','Zip Code','Country','Country Name','Parking Area Name') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+            data_headers = ('Trail Name','Route Type','Trail Difficulty','Rating',
+                            'Review Count','Length (Meters)','Elevation Gain (Meters)','Latitude',
+                            'Longitude','City','State/Region','State/Region Name',
+                            'Zip Code','Country','Country Name','Parking Area Name')
             data_list = []
             for row in all_rows:
-                data_list.append((row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],))
+                data_list.append((
+                    row['ZNAME'],
+                    row['ZROUTETYPENAME'],
+                    row['ZDIFFICULTY'],
+                    row['ZRATING'],
+                    row['ZREVIEWCOUNT'],
+                    row['ZLENGTH'],
+                    row['ZELEVATIONGAIN'],
+                    row['ZLATITUDE'],
+                    row['ZLONGITUDE'],
+                    row['ZCITY'],
+                    row['ZREGION'],
+                    row['ZREGIONNAME'],
+                    row['ZPOSTALCODE'],
+                    row['ZCOUNTRY'],
+                    row['ZCOUNTRYNAME'],
+                    row['zparkname'],
+                ))
 
             report.write_artifact_data_table(data_headers, data_list, file_found)
             report.end_artifact_report()
@@ -60,26 +80,26 @@ def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset
             
         else:
             logfunc('No AllTrails - Trail Details data available')
-        
+
         cursor.execute('''
-        Select 
-        datetime(ZUSER.ZCREATIONTIME + 978307200,'unixepoch') as "Creation Timestamp",
-        ZUSER.ZFIRSTNAME,
-        ZUSER.ZLASTNAME,
-        ZUSER.ZUSERNAME,
-        ZPROFILE.ZEMAIL,
-        ZUSER.ZREFERRALLINK,
-        ZLOCATION.ZLATITUDE,
-        ZLOCATION.ZLONGITUDE,
-        ZLOCATION.ZCITY,
-        ZLOCATION.ZREGION,
-        ZLOCATION.ZREGIONNAME,
-        ZLOCATION.ZCOUNTRY,
-        ZLOCATION.ZCOUNTRYNAME,
-        ZLOCATION.ZPOSTALCODE
-        From ZUSER
-        Inner Join ZPROFILE On ZUSER.Z_PK = ZPROFILE.ZUSER
-        Inner Join ZLOCATION On ZUSER.ZLOCATION = ZLOCATION.Z_PK
+            Select 
+            ZUSER.ZCREATIONTIME,
+            ZUSER.ZFIRSTNAME,
+            ZUSER.ZLASTNAME,
+            ZUSER.ZUSERNAME,
+            ZPROFILE.ZEMAIL,
+            ZUSER.ZREFERRALLINK,
+            ZLOCATION.ZLATITUDE,
+            ZLOCATION.ZLONGITUDE,
+            ZLOCATION.ZCITY,
+            ZLOCATION.ZREGION,
+            ZLOCATION.ZREGIONNAME,
+            ZLOCATION.ZCOUNTRY,
+            ZLOCATION.ZCOUNTRYNAME,
+            ZLOCATION.ZPOSTALCODE
+            From ZUSER
+            Inner Join ZPROFILE On ZUSER.Z_PK = ZPROFILE.ZUSER
+            Inner Join ZLOCATION On ZUSER.ZLOCATION = ZLOCATION.Z_PK
         ''')
 
         all_rows = cursor.fetchall()
@@ -88,12 +108,28 @@ def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset
             report = ArtifactHtmlReport('AllTrails - User Info')
             report.start_artifact_report(report_folder, 'AllTrails - User Info')
             report.add_script()
-            data_headers = ('Creation Timestamp','First Name','Last Name','User Name','Email','Referral Link','Latitude','Longitude','City','Region','Region Name','Country','Country Name','Zip Code') # Don't remove the comma, that is required to make this a tuple as there is only 1 element
+            data_headers = ('Creation Timestamp','First Name','Last Name','User Name',
+                            'Email','Referral Link','Latitude','Longitude',
+                            'City','Region','Region Name','Country',
+                            'Country Name','Zip Code')
             data_list = []
             for row in all_rows:
-                timestamp = convert_ts_human_to_utc(row[0])
-                timestamp = convert_utc_human_to_timezone(timestamp,timezone_offset)
-                data_list.append((timestamp,row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13]))
+                data_list.append((
+                    (convert_sqlite_epoch(row['ZCREATIONTIME']), 'datetime'),
+                    row['ZFIRSTNAME'],
+                    row['ZLASTNAME'],
+                    row['ZUSERNAME'],
+                    row['ZEMAIL'],
+                    row['ZREFERRALLINK'],
+                    row['ZLATITUDE'],
+                    row['ZLONGITUDE'],
+                    row['ZCITY'],
+                    row['ZREGION'],
+                    row['ZREGIONNAME'],
+                    row['ZCOUNTRY'],
+                    row['ZCOUNTRYNAME'],
+                    row['ZPOSTALCODE']
+                ))
 
             report.write_artifact_data_table(data_headers, data_list, file_found)
             report.end_artifact_report()
@@ -106,7 +142,6 @@ def get_AllTrails(files_found, report_folder, seeker, wrap_text, timezone_offset
         else:
             logfunc('No AllTrails - User Info data available')
 
-        db.close()
         return
 
 __artifacts__ = {
--- scripts/artifacts/appConduit.py
+++ scripts/artifacts/appConduit.py
@@ -44,8 +44,8 @@ def get_appConduit(files_found, report_folder, seeker, wrap_text, timezone_offse
                 date_time = line_match.group(3, 5, 4)
                 conv_time = ' '.join(date_time)
                 dtime_obj = datetime.datetime.strptime(conv_time, '%b %d %Y %H:%M:%S')
-                dtime_obj = convert_time_obj_to_utc(dtime_obj)
-                dtime_obj = convert_utc_human_to_timezone(dtime_obj, timezone_offset)
+                #dtime_obj = convert_time_obj_to_utc(dtime_obj)
+                #dtime_obj = convert_utc_human_to_timezone(dtime_obj, timezone_offset)
                 
                 values  = line_match.group(9)
                 device_id = line_match.group(11)
@@ -53,10 +53,20 @@ def get_appConduit(files_found, report_folder, seeker, wrap_text, timezone_offse
                 if 'devicesAreNowConnected' in values:
                     device_type_and_info.append((device_id,line_match.group(12).split(" ")[4],line_match.group(12).split(" ")[5]))
                     info = 'Connected'
-                    data_list.append((dtime_obj,info,device_id,file_name))
+                    data_list.append((
+                        (dtime_obj, 'datetime'),
+                        info,
+                        device_id,
+                        file_name
+                    ))
                 if 'devicesAreNoLongerConnected' in values:
                     info = 'Disconnected'
-                    data_list.append((dtime_obj,info,device_id,file_name))
+                    data_list.append((
+                        (dtime_obj, 'datetime'),
+                        info,
+                        device_id,
+                        file_name
+                    ))
                 # if 'Resuming because' in values:
                 #     info = 'Resumed'
                 #     data_list.append((dtime_obj,info,device_id,device_type_tmp,file_name))
@@ -70,10 +80,12 @@ def get_appConduit(files_found, report_folder, seeker, wrap_text, timezone_offse
     device_type_and_info = list(set(device_type_and_info))
 
     data_headers_device_info = ('Device ID', 'Device type and version', 'Device extra information')
-    data_headers = ('Time', 'Device interaction', 'Device ID', 'Log File Name')   
+    data_headers = ('Timestamp', 'Device interaction', 'Device ID', 'Log File Name')
 
     report = ArtifactHtmlReport('App Conduit')
-    report.start_artifact_report(report_folder, 'App Conduit', 'The AppConduit log file stores information about interactions between iPhone and other iOS devices, i.e. Apple Watch')
+    report.start_artifact_report(report_folder, 'App Conduit',
+                 'The AppConduit log file stores information about interactions between iPhone and '
+                                 'other iOS devices, i.e. Apple Watch')
     
     report.add_script()
     source_files_found = ', '.join(source_files)
